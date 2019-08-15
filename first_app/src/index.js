8f77bee22540783efb2235c76ec7d8355d55d3ab
Van Sang <vansang1706@gmail.com>
2019-08-15 22:59:19 +0700

    Introducing JSX

--- first_app/src/index.js
+++ first_app/src/index.js
@@ -4,8 +4,59 @@ import ReactDOM from 'react-dom';
 //import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// ex 1
+const element = <h1>Hello, world!</h1>;
+
+// ex 2
+var name = 'Van Sang';
+const element2 = <h1>Hello, {name}!</h1>
+
+// ex 3
+function formatName(urs){
+    return urs.firstName + ' ' + urs.lastName;
+}
+const user = {
+    firstName: 'Sang',
+    lastName: 'Le'
+}
+const element3 = <h1>{formatName(user)}</h1>;
+
+// ex 4
+const user2 ={
+    firstName: 'Than',
+    lastName: 'Huynh'
+}
+function getGreeting(urs){
+    if(urs){
+        return <h1>Hello, {formatName(urs)}!</h1>;
+    }
+    return <h1>Hello, Stranger.</h1>
+}
+
+// ex 5
+const element5 = React.createElement(
+    'h1',
+    {className: 'greeting'},
+    'Hello, ex 5!'
+);
+
+// ex 6
+const element6 = {
+    type: 'h1',
+    props: {
+        className: 'greeting',
+        children: 'Hello, ex 6!'
+    }
+};
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    //<h1>Hello, world!</h1>,
+    //element,
+    //element2,
+    //element3,
+    //getGreeting(user2),
+    element5,
+    //element6, // doesn't work
     document.getElementById('root')
 );
 
